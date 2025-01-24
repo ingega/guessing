e0dcbf9949e36b7c4c7c9b9300139730c59cf77a
Edmundo Garcia <ingega@gmail.com>
2025-01-24 14:53:06 +0000

    fix: adding the rest of code

--- number_guess.sh
+++ number_guess.sh
@@ -25,5 +25,40 @@ MAIN(){
   fi
   # Generate a random number between 1 and 1000
   RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
+  # if NaN then re-promt
+  SECOND_FLAG=true
+  ATTEMPTS=1 # there's at least one shoot
+  while $SECOND_FLAG
+  do
+    echo "Guess the secret number between 1 and 1000:"
+    read GUESS
+    if ! [[ $GUESS =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+      read GUESS
+    else
+      # is a number, check if low, high or match
+      while true
+      do
+        if [[ $GUESS -eq $RANDOM_NUMBER ]]
+        then
+          echo "You guessed it in $ATTEMPTS tries. The secret number was $RANDOM_NUMBER. Nice job!"
+          # add to the db
+          ADD_GAME=$($PSQL "INSERT INTO games(user_id, attempts) VALUES($FIND_USER_ID, $ATTEMPTS)")
+          break
+        else
+          if [[ $GUESS -lt $RANDOM_NUMBER ]]
+          then
+            echo "It's higher than that, guess again:"
+            read GUESS
+          else
+            echo "It's lower than that, guess again:"
+            read GUESS
+          fi
+          ATTEMPTS=$((ATTEMPTS + 1))
+        fi
+      done
+    fi
+  done
 }
 MAIN
